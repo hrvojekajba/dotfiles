95a91745670750d65260d586418424afd29c5084
Hrvoje <172497559+bluzmija@users.noreply.github.com>
2025-07-01 20:35:40 +0200

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -80,9 +80,34 @@ return {
     },
   },
   {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
+    priority = 1000,
+    config = function()
+      require("dracula").setup({
+        transparent_bg = true,
+      })
+    end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+      -- vim.cmd("colorscheme github-dark")
+      vim.cmd("colorscheme github_dark") -- if you want to swap to another theme, DELETE THIS
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "kanso",
     },
   },
 }
