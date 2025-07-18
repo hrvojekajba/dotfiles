85188c80557483ac074820741e809a03f6133190
bluzmija <172497559+bluzmija@users.noreply.github.com>
2025-07-18 16:58:49 +0200

    Setup conform.nvim

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,4 +2,10 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	enabled = true,
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			indent = { enable = true},
+		})
+	end
 }
